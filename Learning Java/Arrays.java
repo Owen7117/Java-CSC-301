ac221f7a26744e46644b6a9f3c14636d473abf45
Owen7117 <owenmoneil2005@gmail.com>
2025-08-28 13:19:03 -0400

    notes

---
+++ Learning Java/Arrays.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Arrays{
+    /*
+    public static void main(String[] args){
+        char[] myChars;
+        int[] myNums = new int[5];
+        int[][] mygrid = new int[3][4];
+        boolean[] flips = {true, false, false, true};
+        System.out.println("How many?");
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        String[] s = new Strings[i];
+    }
+
+    public static void main(String[] args){
+        int[] a = {1,2,3};
+        int[] b = {1,2,3};
+
+        if(a.equals(b)) System.out.println("equal");
+        else System.out.println("not equal");
+
+        if(a==b) System.out.println("the same");
+        else System.out.println("not the same");
+
+        if(Arrays.equals(a,b)) System.out.println("equal contents");
+        else System.out.println("not equal contents");
+
+        String s = "hi";
+        String t = new String("hi");
+        String s = t + 7
+    }
+    */
+    public static void main(String[] args){
+        String a = "hi";
+        Strin b = "hello";
+
+        System.out.println("a == b? " + a.equals(b));
+        System.out.println("How do they compare?");
+        System.out.println(a.compareTo(b));
+    }
+}
